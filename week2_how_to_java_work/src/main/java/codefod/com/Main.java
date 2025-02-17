f703f6147cab8247aab3aaae9c9a179a9d67b2b2
giakinh0823 <giakinh2000@gmail.com>
2025-02-17 09:26:19 +0700

    update assignment

--- week2_how_to_java_work/src/main/java/codefod/com/Main.java
+++ week2_how_to_java_work/src/main/java/codefod/com/Main.java
@@ -3,6 +3,8 @@ package codefod.com;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // TODO: Viết một chương trình Java cơ bản và thực chạy compile file chương trình ra bytecode
+        // TODO: Thực chạy file bytecode bằng lệnh java
+        // TODO: Thực hiện chạy file bytecode với cấu hình heap size, gc, ...
     }
 }
